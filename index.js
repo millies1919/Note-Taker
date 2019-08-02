af632d7a9b5b60175c890957a9bb23bbe998f34b
Matt Illies <mattillies.webbie@gmail.com>
2019-08-02 00:22:01 -0500

    Add button added/delete started
    Add button adds new containers

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const deleteButton = () => {
-
+const deleteButton = (elementId) => {
+        var deleted = document.getElementById(elementId);
+        deleted.parentNode.removeChild(deleted);
 }
 
 const saveButton = () => {
 
 }
-
+var counter = 1;
 const addButton = () => {
-    var newCard = document.getElementById("container-template")
+    var newCard = document.getElementById("container-template");
     var cln = newCard.cloneNode(true);
     document.getElementById("body").appendChild(cln);
+    counter++;
+    cln.id = "container" + counter
 }
