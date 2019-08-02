3474a79708423c2e7535659dc588533428f6ceb5
Matt Illies <mattillies.webbie@gmail.com>
2019-08-02 01:14:17 -0500

    Add button moved outside, delete button working

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const deleteButton = (elementId) => {
-        var deleted = document.getElementById(elementId);
-        deleted.parentNode.removeChild(deleted);
+var counter = 0;
+const deleteButton = (element) => {
+        var deleted = element
+        var parent = deleted.parentNode;
+        parent.parentNode.removeChild(parent);
 }
 
 const saveButton = () => {
 
 }
-var counter = 1;
 const addButton = () => {
     var newCard = document.getElementById("container-template");
     var cln = newCard.cloneNode(true);
