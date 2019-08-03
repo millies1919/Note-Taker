35aef11038831cca01b835cf2d54f2c11c2b8fab
Matt Illies <mattillies.webbie@gmail.com>
2019-08-02 23:55:20 -0500

    Destructured addButton functionality

--- index.js
+++ index.js
@@ -9,30 +9,38 @@ const onLoad = () => {
 }
 
 const deleteButton = (element) => {
-        var deleted = element;
-        var parent = deleted.parentNode;
-        parent.parentNode.removeChild(parent);
+    var deleted = element;
+    var parent = deleted.parentNode;
+    parent.parentNode.removeChild(parent);
 }
 
-const saveButton = () => {
-
+const saveButton = (element) => {
+    title = document.getElementsByClassName("title2")
+    console.log(title)
 }
 
 const addButton = () => {
+    createNote();
+}
+
+const createNote = () => {
     var counter = 0;
     var newCard = document.getElementById("container-template");
     var cln = newCard.cloneNode(true);
     document.getElementById("body").appendChild(cln);
     document.getElementById("containerdiv").appendChild(cln);
+    console.log(cln.children)
+    
+    
     
     var newId = undefined;
     var element = undefined;
 
     do {
-    counter++
-    newId = "container" + counter;
-    element = document.getElementById(newId);
+        counter++
+        newId = "container" + counter;
+        element = document.getElementById(newId);
 
     } while(element != undefined)
     cln.id = newId
-}   
+}
